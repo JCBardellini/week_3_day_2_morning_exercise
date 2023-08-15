854775d4432ffe26123b6da693db7131417f30a8
Jean Bardellini <jbardellini@LABA.PSWAS.LOCAL>
2023-08-15 13:03:32 -0700

    almost done

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// const checkEvenOrOdd = (num) => {
+//     if ( num % 2 === 0) {
+//         return "It is even"
+//     } else {
+//         return 'it is odd'
+//     }
+// }
+// console.log(checkEvenOrOdd(8))
+
+// const sumNumbers = (numOne, numTwo) => {
+//     return numOne + numTwo
+// }
+
+// console.log(sumNumbers(20, 4))
+
+// const drinkEnoughCoffee = (amountOfCups) => {
+//     if (amountOfCups >= 4) {
+//         return "wohoo, you reach the limit"
+//     } else {
+//         return "you need more coffee to function"
+//     }
+// }
+
+// console.log(drinkEnoughCoffee(2))
+
+// write a function to check how many words the sentence has
+
+const wordCount = (sentence) => {
+    
+}
